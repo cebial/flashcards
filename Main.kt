e799c5ef3c7f619a3f948532a79877609e8afa7d
cebial <biglazypanda@gmail.com>
2022-06-04 10:15:35 +0200

    Change for stage 5/7: Menu, please

--- Main.kt
+++ Main.kt
@@ -1,48 +1,104 @@
 package flashcards
 
+import java.io.File
+import kotlin.random.Random
+
 class CardBox {
-    val cards = mutableMapOf<String, String>()
+    private val cards = mutableMapOf<String, String>()
 
-    fun inputCards() {
-        println("Input the number of cards:")
-        val count = readln().toInt()
+    init {
+        run()
+    }
 
-        for (i in 1..count) {
-            println("Card #$i:")
-            var term = readln()
-            while (term in cards) {
-                println("The term \"$term\" already exists. Try again:")
-                term = readln()
-            }
+    private fun getKey(value: String): String = cards.filterValues { it == value }.keys.first()
 
-            println("The definition for card #$i:")
-            var definition = readln()
-            while (definition in cards.values) {
-                println("The definition \"$definition\" already exists. Try again:")
-                definition = readln()
-            }
+    fun <K,V> Map<K,V>.random(): Map.Entry<K,V> = entries.elementAt(Random.nextInt(size))
 
-            cards[term] = definition
+    fun addCard() {
+        println("The card:")
+        var term = readln()
+        if (term in cards) {
+            println("The card \"$term\" already exists.")
+            return
+        }
+
+        println("The definition of the card:")
+        var definition = readln()
+        if (definition in cards.values) {
+            println("The definition \"$definition\" already exists.")
+            return
+        }
+
+        cards[term] = definition
+        println("The pair (\"$term\":\"$definition\") has been added.")
+    }
+
+    fun removeCard() {
+        println("Which card?")
+        var term = readln()
+        if (term in cards) {
+            cards.remove(term)
+            println("The card has been removed.")
+        } else println("Can't remove \"$term\": there is no such card.")
+    }
+
+    fun takeQuiz() {
+        println("How many times to ask?")
+        repeat(readln().toInt()) {
+            val card = cards.random()
+            println("Print the definition of \"${card.key}\":")
+            var answer = readln()
+            println(when {
+                card.value == answer -> "Correct!"
+                answer in cards.values -> "Wrong. The right answer is \"${card.value}\", but your definition is correct for \"${getKey(answer)}\"."
+                else -> "Wrong. The right answer is \"${card.value}\"."
+            })
         }
     }
 
-    fun getKey(value: String): String = cards.filterValues { it == value }.keys.first()
+    fun import() {
+        println("File name:")
+        val file = File(readln())
 
-    fun takeQuiz() {
-        cards.forEach {
-            println("Print the definition of \"${it.key}\":")
-            var answer = readln()
-            println(when {
-                it.value == answer -> "Correct!"
-                answer in cards.values -> "Wrong. The right answer is \"${it.value}\", but your definition is correct for \"${getKey(answer)}\"."
-                else -> "Wrong. The right answer is \"${it.value}\"."
-            })
+        if (!file.exists()) {
+            println("File not found.")
+            return
+        }
+
+        val lines = file.readLines()
+        lines.forEach {
+            val (key, value) = it.split('=')
+            cards[key] = value
+        }
+        println("${lines.size} cards have been loaded.")
+    }
+
+    fun export() {
+        println("File name:")
+        val filename = readln()
+
+        File(filename).writeText(cards.entries.joinToString("\n"))
+        println("${cards.size} cards have been saved.")
+    }
+
+    fun run() {
+        while (true) {
+            println("Input the action (add, remove, import, export, ask, exit):")
+            val action = readln()
+            when (action) {
+                "add" -> addCard()
+                "remove" -> removeCard()
+                "import" -> import()
+                "export" -> export()
+                "ask" -> takeQuiz()
+                "exit" -> return
+            }
+            println()
         }
     }
 }
 
 fun main() {
-    val box = CardBox()
-    box.inputCards()
-    box.takeQuiz()
+    CardBox()
+    println("Bye bye!");
 }
